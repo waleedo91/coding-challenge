eef2f46c128bf2ee2a392a60182d8e1e0b0df4f8
Waleed <wsaleh1991@gmail.com>
2021-12-10 00:05:17 -0600

    almost there

--- index.js
+++ index.js
@@ -16,59 +16,65 @@ d3.csv(csvData).then(function (data) {
   newArr.forEach(countFunc);
   // console.log(countObj);
 
-  function searchResArr(obj) {
-    const res = [];
-    const keys = Object.keys(obj);
-    keys.forEach((key) => {
-      res.push({ key: obj[key] });
-    });
-    return res;
-  }
-  console.log(searchResArr(countObj));
-});
+  const searched = [];
+  const popular = [];
+  const count = Object.entries(countObj).map((e) => ({ [e[0]]: e[1] }));
+  for (let i = 0; i < count.length; i++) {
+    searched.push(Object.keys(count[i]));
+    popular.push(Object.values(count[i]));
 
-// setup
-const data = {
-  labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-  datasets: [
-    {
-      label: "Weekly Sales",
-      data: [18, 12, 20, 9, 12, 3, 9],
-      backgroundColor: [
-        "rgba(255, 26, 104, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-        "rgba(0, 0, 0, 0.2)",
+    // setup
+    const barChart = {
+      labels: searched,
+      datasets: [
+        {
+          label: "Weekly Sales",
+          data: popular,
+          backgroundColor: [
+            "rgba(255, 26, 104, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(0, 0, 0, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 26, 104, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(0, 0, 0, 1)",
+          ],
+          borderWidth: 1,
+        },
       ],
-      borderColor: [
-        "rgba(255, 26, 104, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-        "rgba(0, 0, 0, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+    };
 
-// config
-const config = {
-  type: "bar",
-  data,
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+    // config
+    const config = {
+      type: "bar",
+      data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
       },
-    },
-  },
-};
+    };
 
-// render init block
-const myChart = new Chart(document.getElementById("myChart"), config);
+    // render init block
+    let myChart = new Chart(
+      document.getElementById("myChart").getContext("2d"),
+      config
+    );
+    myChart.update();
+    myChart = new Chart(
+      document.getElementById("myChart").getContext("2d"),
+      config
+    );
+  }
+});
