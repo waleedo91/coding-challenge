b88f04cbdff5092be7f38bebaac04a4fe9cd003a
Waleed <wsaleh1991@gmail.com>
2021-12-12 03:35:29 -0600

    Got everything to work but all results stacking ontop of eachother a bit hard to read results.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let notSo = [];
 let popularLabel = [];
 let notSoLabel = [];
 
-// Input function to upload CSV file
+// Upload CSV file and parses to JSON using papaParser
 const uploadSubmit = document
   .getElementById("upload-submit")
   .addEventListener("click", () => {
@@ -12,53 +12,92 @@ const uploadSubmit = document
       header: true,
       skipEmptyLines: true,
       complete: function (results) {
-        console.log(results.data.map((hit) => hit.hits));
-        let output = Object.values(
-          results.data.reduce((obj, { query }) => {
-            if (obj[query] === undefined) {
-              obj[query] = {
-                query: query,
-                occurrences: 1,
-              };
-            } else {
-              obj[query].occurrences++;
-            }
-            return obj;
-          }, {})
-        );
+        let shortRes = {};
+        let newQuery = [];
+        results.data.forEach((obj) => {
+          shortRes = {
+            query: obj.query,
+            hits: obj.hits,
+          };
+          newQuery.push(shortRes);
+        });
 
-        output.map((item) => {
-          //   console.log(item);
-          if (item.occurrences === 1) {
+        // Counts the occurrences of each search query
+        function occurrencesCount(arr, key, hits) {
+          let arr2 = [];
+
+          newQuery.forEach((x) => {
+            if (
+              arr2.some((val) => {
+                return val[key] == x[key];
+              })
+            ) {
+              arr2.forEach((k) => {
+                if (k[key] === x[key]) {
+                  k["occurrence"]++;
+                }
+              });
+            } else {
+              let a = {};
+              a[key] = x[key];
+              a[hits] = x[hits];
+              a["occurrence"] = 1;
+              arr2.push(a);
+            }
+          });
+          return arr2;
+        }
+
+        let key = "query";
+        let hits = "hits";
+        const updatedRes = occurrencesCount(newQuery, key, hits);
+
+        // Separates a query from ones with 0 hits and the ones with hits.
+        updatedRes.forEach((item) => {
+          if (item.hits === "0") {
             notSo.push(item);
             notSoLabel.push(item.query);
+            return;
           } else {
             popular.push(item);
             popularLabel.push(item.query);
+            return;
           }
         });
       },
     });
   });
 
+// Chart setup beyond this point.
 const data = {
   labels: [],
   datasets: [
     {
-      label: "# of Votes",
-      borderWidth: 5,
-      backgroundColor: "rgb(128, 255, 0)",
-      borderColor: "black",
+      label: "Searched",
+      borderWidth: 1,
+      backgroundColor: "black",
+      borderColor: "white",
       spacing: 15,
-      hoverBackgroundColor: "white",
+      hoverBackgroundColor: "red",
+      hoverBorderWidth: 3,
+      cutout: "50%",
     },
   ],
 };
 
 const config = {
-  type: "pie",
+  type: "doughnut",
   data,
   options: {
+    scales: {
+      y: {
+        grid: {
+          offset: true,
+        },
+      },
+    },
+    indexAxis: "y",
+    spacing: 1,
     plugins: {
       legend: {
         display: false,
@@ -73,16 +112,16 @@ const config = {
   },
 };
 
-// Setup for Chart.
 const myChart = new Chart(document.getElementById("myChart"), config);
 
+// Adds event to Popular and Random button
 function updateChart(label) {
   if (label === "popular") {
     myChart.data.labels = popularLabel.map((name) => {
       return name;
     });
     myChart.data.datasets[0].data = popular.map((searches) => {
-      return searches.occurrences;
+      return searches.occurrence;
     });
   }
   if (label === "notSo") {
@@ -90,7 +129,7 @@ function updateChart(label) {
       return name;
     });
     myChart.data.datasets[0].data = notSo.map((searches) => {
-      return searches.occurrences;
+      return searches.occurrence;
     });
   }
   myChart.update();
